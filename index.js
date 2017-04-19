8d7148f12d89a7a34990664d9e1f2cd6e7a8f78d
Jesse Hull <jessehull@gmail.com>
2017-04-19 12:03:35 -0700

    remove async await

--- index.js
+++ index.js
@@ -5,33 +5,34 @@ const request = require('request-promise')
 const moment = require('moment-timezone')
 
 class Scraper {
-  async nextLaunches(url = 'https://spaceflightnow.com/launch-schedule/') {
-    const data = await request(url)
-    const $ = cheerio.load(data)
-    const ret = []
-    $('.datename').each((i, elem) => {
-      const launchDate = $('.launchdate', elem).text().trim()
-      const missionParts = $('.mission', elem).text().trim()
-      const parts = missionParts.split('•')
-      const rocket = parts[0].trim()
-      const mission = parts[1].trim()
-      const missionDataElem = $(elem).next('.missiondata')
-      const missionDataHtml = $(missionDataElem).html()
-      const launchTime = missionDataHtml.split('span>')[1].split('<span')[0].trim()
-      const launchSite = missionDataHtml.split('span>')[2].trim()
-      const missionDescription = $(missionDataElem).next('.missdescrip').text()
-      const launchDatetime = this.getLaunchDatetime(launchDate, launchTime)
-      ret[i] = {
-        launchDate,
-        rocket,
-        mission,
-        launchTime,
-        launchSite,
-        missionDescription,
-        launchDatetime
-      }
+  nextLaunches(url = 'https://spaceflightnow.com/launch-schedule/') {
+    return request(url).then(data => {
+      const $ = cheerio.load(data)
+      const ret = []
+      $('.datename').each((i, elem) => {
+        const launchDate = $('.launchdate', elem).text().trim()
+        const missionParts = $('.mission', elem).text().trim()
+        const parts = missionParts.split('•')
+        const rocket = parts[0].trim()
+        const mission = parts[1].trim()
+        const missionDataElem = $(elem).next('.missiondata')
+        const missionDataHtml = $(missionDataElem).html()
+        const launchTime = missionDataHtml.split('span>')[1].split('<span')[0].trim()
+        const launchSite = missionDataHtml.split('span>')[2].trim()
+        const missionDescription = $(missionDataElem).next('.missdescrip').text()
+        const launchDatetime = this.getLaunchDatetime(launchDate, launchTime)
+        ret[i] = {
+          launchDate,
+          rocket,
+          mission,
+          launchTime,
+          launchSite,
+          missionDescription,
+          launchDatetime
+        }
+      })
+      return ret
     })
-    return ret
   }
 
   getLaunchDatetime(launchDate, launchTime) {
